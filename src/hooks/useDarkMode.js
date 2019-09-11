6bf2e0fd7b2ff178dd1f0a11bfa159945754528e
Eric <eric@Erics-MacBook-Pro.local>
2019-09-10 22:23:24 -0500

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useEffect} from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [ darkMode, setDarkMode] = useLocalStorage('clicked');
+    useEffect(() => {
+        darkMode ?
+        document.body.classList.add('dark-mode')
+        :
+        document.body.classList.remove('dark-mode');
+    }
+    , [darkMode])
+    return [darkMode, setDarkMode]
+}
+
+
